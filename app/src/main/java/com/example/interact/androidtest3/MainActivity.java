0ee6333d5e4d44cc733518382a77955818523148
InterAct <oy9@ya.ru>
2015-03-15 20:36:57 +0200

    Init

--- app/src/main/java/com/example/interact/androidtest3/MainActivity.java
+++ app/src/main/java/com/example/interact/androidtest3/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present. ok
+        //test
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
