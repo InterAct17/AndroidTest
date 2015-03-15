dda4bf12fdb07b1d38e73da9a3a657cd70ea39c0
InterAct <oy9@ya.ru>
2015-03-15 20:52:04 +0200

    Init

--- app/src/main/java/com/example/interact/androidtest3/MainActivity.java
+++ app/src/main/java/com/example/interact/androidtest3/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends Activity {
         ///Yes
         //Some release fix
         // some features
+        //Upgrades
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
